591042f5f1a94f8664eceac5019b6eda9fa25da3
Alfie Nurse <alfienurse@gmail.com>
2025-01-10 00:13:41 +0000

    Finishing: clean up MainActivity, the navigation stuff was logic I defined at the start and never used because my navigation for the user-SIDE is done via NavigationManager and the bottom_nav_menu; I explain in the report

--- app/src/main/java/com/example/staffsyncapp/MainActivity.java
+++ app/src/main/java/com/example/staffsyncapp/MainActivity.java
@@ -15,17 +15,23 @@ import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.fragment.NavHostFragment;
 
+/**
+ * MainActivity: Entry-point for the APP
+
+ * The class serves as the main activity
+ * - Sets initial user interface; links the Java to XML files, respectively
+ * - Configures the app to run in full-screen mode(removes purple heading)
+ * - Handles run-time permissions for Android 13+; asks to grant notifications
+ */
+
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) { // on create 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN); // full screen
-
-        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.nav_host_fragment);
-        NavController navController = navHostFragment.getNavController();
+        // full-screen mode
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN);
 
         // request permissions for build numbers 13+
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
